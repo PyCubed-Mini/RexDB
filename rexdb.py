05c49e0a0fc762c81c2ce7dcc8e28c796a9d26fa
tomyyyD <thomas5d@comcast.net>
2023-02-05 23:37:05 -0500

    Created the pack and unpack functions
    data can be packed and unpacked based on a map that is created when DensePacker is initialized.

--- rexdb.py
+++ rexdb.py
@@ -3,20 +3,68 @@ import struct
 
 class DensePacker():
     def __init__(self, fstring: str) -> None:
-        self.make_format(fstring)
+        self.fstring_length = len(fstring)
+        self.user_fstring = fstring
+        self.dense_fstring = self.make_format(fstring)
+        self.user_dense_map = self.create_pack_maps(self.user_fstring, self.dense_fstring)
 
-        # formats for space c: char, i: int32, f: float, ?: bool, h: int16
+    '''
+    make_format: str -> str
+    
+    orders the incoming format string based on the byte size of
+    the data type. 
+    icfc -> ficc
+    '''
+
     def make_format(self, fstring: str) -> str:
-        charList = {"c": 1, "?": 0.1, "h": 2, "i": 4, "f": 4.1}
-        denseFormat = list(self._user_fstring)
-        denseFormat.sort(key=(lambda c: charList[c]), reverse=True)
+        char_list = {"c": 1, "?": 0.1, "h": 2, "i": 4, "f": 4.1}
+        denseFormat = list(fstring)
+        denseFormat.sort(key=(lambda c: char_list[c]), reverse=True)
         return ''.join(denseFormat)
 
+    '''
+    create_pack_map : str * str -> str * str
+
+    Creates a map from the user_fstring to the dense_fstring.
+    This maps the last occurence of the data type in user_fstring
+    to the first occurence in dense_fstring.
+    For Example icfc = [0, 1, 2, 3] -> ficc = [2, 0, 3, 1]
+        Notice how the "c" at index 3 comes before the "c" at 
+        index 1 
+    '''
+
+    def create_pack_maps(self, user_fstring, dense_fstring) -> str:
+        pack_map = {"c": [], "?": [], "h": [], "i": [], "f": []}
+        length = len(user_fstring)
+        # map from the user string to the dense string
+        user_dense_map = [0] * length
+        for i in range(length):
+            pack_map[user_fstring[i]].append(i)
+        for i in range(length):
+            user_dense_map[i] = pack_map[dense_fstring[i]].pop()
+
+        return user_dense_map
+
+    '''
+    pack: tuple -> tuple
+
+    takes input in the user_format and will output that data in the 
+    dense_format
+    '''
+
     def pack(self, data: tuple) -> tuple:
-        pass
+        result = [0] * self.fstring_length
+        for i in range(self.fstring_length):
+            index = self.user_dense_map[i]
+            result[i] = data[index]
+        return tuple(result)
 
     def unpack(self, data: tuple) -> tuple:
-        pass
+        result = [0] * self.fstring_length
+        for i in range(self.fstring_length):
+            index = self.user_dense_map[i]
+            result[index] = data[i]
+        return tuple(result)
 
 
 class RexDB:
@@ -24,7 +72,8 @@ class RexDB:
     def __init__(self, file, fstring, lines=100, cursor=0):
         self._fd = open(file, "wb")
         self._user_fstring = fstring
-        self._dense_fstring = self.make_format()
+        packer = DensePacker(fstring)
+        self._dense_fstring = packer.dense_fstring
         self._line_size = struct.calcsize(self._dense_fstring)
         self._cursor = cursor
         self._file = file
--- tests/test_dense_packer.py
+++ tests/test_dense_packer.py
@@ -1,6 +1,6 @@
 from pyfakefs import fake_filesystem_unittest
 
-from rexdb import RexDB
+from rexdb import RexDB, DensePacker
 
 
 class DensePacketTest(fake_filesystem_unittest.TestCase):
@@ -13,3 +13,18 @@ class DensePacketTest(fake_filesystem_unittest.TestCase):
 
         db = RexDB("test3.db", "cifh?c", lines=1)
         self.assertEqual(db._dense_fstring, "fihcc?")
+
+    def testPacker(self):
+        packer = DensePacker("icfc")
+        assert (packer.dense_fstring == "ficc")
+        assert (packer.user_dense_map == [2, 0, 3, 1])
+        assert (packer.pack((32, b'f', 8.9, b'p')) == (8.9, 32, b'p', b'f'))
+        assert (packer.unpack((8.9, 32, b'p', b'f')) == (32, b'f', 8.9, b'p'))
+
+        packer = DensePacker("fcichf?fci")
+        assert (packer.dense_fstring == "fffiihccc?")
+        assert (packer.user_dense_map == [7, 5, 0, 9, 2, 4, 8, 3, 1, 6])
+        assert (packer.pack((9.2, b'l', 1234, b'p', 1, 9.1, True, 1.1, b'm', 4321))
+                == (1.1, 9.1, 9.2, 4321, 1234, 1, b'm', b'p', b'l', True))
+        assert (packer.unpack((1.1, 9.1, 9.2, 4321, 1234, 1, b'm', b'p', b'l', True))
+                == (9.2, b'l', 1234, b'p', 1, 9.1, True, 1.1, b'm', 4321))
