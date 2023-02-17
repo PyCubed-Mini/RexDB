02393257f06fe0c1e1e5f4611ed9a92ee1e43147
thetazero <lesha.seletskiy@gmail.com>
2023-02-16 20:13:39 -0500

    Add tests for edge cases in dense packer f-string (#4)

--- tests/test_dense_packer.py
+++ tests/test_dense_packer.py
@@ -14,7 +14,18 @@ class DensePacketTest(fake_filesystem_unittest.TestCase):
         db = RexDB("test3.db", "cifh?c", lines=1)
         self.assertEqual(db.packer.dense_fstring, "fihcc?")
 
+    def testEdgeCases(self):
+        with self.assertRaises(ValueError):
+            DensePacker("")
+        with self.assertRaises(ValueError):
+            DensePacker("goose")
+
     def testPacker(self):
+        packer = DensePacker("i")
+        assert (packer.dense_fstring == "i")
+        assert (packer.user_dense_map == [0])
+        assert (packer.unpack(packer.pack((32,))) == (32,))
+
         packer = DensePacker("icdc")
         assert (packer.dense_fstring == "dicc")
         assert (packer.user_dense_map == [2, 0, 3, 1])
