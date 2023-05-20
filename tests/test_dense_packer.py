0ef29abd36e1ab3c4853e945d41e1a4e99962cd8
tomyyyD <thomas5d@comcast.net>
2023-05-19 20:31:54 -0400

    cleaning up test cases

--- tests/test_dense_packer.py
+++ tests/test_dense_packer.py
@@ -23,25 +23,25 @@ class DensePacketTest(fake_filesystem_unittest.TestCase):
 
     def testPacker(self):
         packer = DensePacker("i")
-        assert (packer.dense_fstring == "i")
-        assert (packer.user_dense_map == [0])
-        assert (packer.unpack(packer.pack((32,))) == (32,))
+        self.assertEqual(packer.dense_fstring, "i")
+        self.assertEqual(packer.user_dense_map, [0])
+        self.assertEqual(packer.unpack(packer.pack((32,))), (32,))
 
         packer = DensePacker("icdc")
-        assert (packer.dense_fstring == "dicc")
-        assert (packer.user_dense_map == [2, 0, 3, 1])
-        assert (packer.unpack(packer.pack((32, b'f', 8.9, b'p'))) == (32, b'f', 8.9, b'p'))
+        self.assertEqual(packer.dense_fstring, "dicc")
+        self.assertEqual(packer.user_dense_map, [2, 0, 3, 1])
+        self.assertEqual(packer.unpack(packer.pack((32, b'f', 8.9, b'p'))), (32, b'f', 8.9, b'p'))
 
         packer = DensePacker("dcichd?dci")
-        assert (packer.dense_fstring == "dddiihccc?")
-        assert (packer.user_dense_map == [7, 5, 0, 9, 2, 4, 8, 3, 1, 6])
-        assert (packer.unpack(packer.pack((9.2, b'l', 1234, b'p', 1, 9.1, True, 1.1, b'm', 4321)))
-                == (9.2, b'l', 1234, b'p', 1, 9.1, True, 1.1, b'm', 4321))
+        self.assertEqual(packer.dense_fstring, "dddiihccc?")
+        self.assertEqual(packer.user_dense_map, [7, 5, 0, 9, 2, 4, 8, 3, 1, 6])
+        self.assertEqual(packer.unpack(packer.pack((9.2, b'l', 1234, b'p', 1, 9.1, True, 1.1, b'm', 4321))),
+                         (9.2, b'l', 1234, b'p', 1, 9.1, True, 1.1, b'm', 4321))
 
         packer = DensePacker("dcichd?dci")
-        assert (packer.dense_fstring == "dddiihccc?")
-        assert (packer.user_dense_map == [7, 5, 0, 9, 2, 4, 8, 3, 1, 6])
-        assert (packer.unpack(packer.pack((9.2, b'l', 1234, b'p', 1, 9.1, True, 1.1, b'm', 4321)))
-                == (9.2, b'l', 1234, b'p', 1, 9.1, True, 1.1, b'm', 4321))
-        assert (packer.unpack(packer.pack((9.1, b'p', 6534, b'p', 0, 1.9, True, 4.5, b'k', 12345)))
-                == (9.1, b'p', 6534, b'p', 0, 1.9, True, 4.5, b'k', 12345))
+        self.assertEqual(packer.dense_fstring, "dddiihccc?")
+        self.assertEqual(packer.user_dense_map, [7, 5, 0, 9, 2, 4, 8, 3, 1, 6])
+        self.assertEqual(packer.unpack(packer.pack((9.2, b'l', 1234, b'p', 1, 9.1, True, 1.1, b'm', 4321))),
+                         (9.2, b'l', 1234, b'p', 1, 9.1, True, 1.1, b'm', 4321))
+        self.assertEqual(packer.unpack(packer.pack((9.1, b'p', 6534, b'p', 0, 1.9, True, 4.5, b'k', 12345))),
+                         (9.1, b'p', 6534, b'p', 0, 1.9, True, 4.5, b'k', 12345))
