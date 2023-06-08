78d66a5ceee0d99dfca020d933426732b4e3b218
tomyyyD <thomas5d@comcast.net>
2023-06-08 13:43:57 -0400

    unit tests uses faketime

--- tests/test_threshold_query.py
+++ tests/test_threshold_query.py
@@ -1,18 +1,19 @@
 from pyfakefs import fake_filesystem_unittest
 from src.rexdb import RexDB
 import random
-import time
+from tests.faketime import FakeTime
 
 
 class QueryTests(fake_filesystem_unittest.TestCase):
     def setUp(self) -> None:
         self.setUpPyfakefs()
+        self.time = FakeTime()
 
     def test_query_basic(self):
         db = RexDB("ifc", ("index", "super precise number", "initial"), 26, 2)
         for i in range(50):
             db.log((i, random.random(), bytes(chr(i % 26 + 65), "utf-8")))
-            time.sleep(0.5)
+            self.time.sleep(0.5)
 
         data = db.get_data_at_field_threshold("index", 40, 1)
 
